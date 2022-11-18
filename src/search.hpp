ffcc59b523db4a72d8432ba74649d6d1b4762eb6
mato <31648945+matoototo@users.noreply.github.com>
2022-11-18 21:00:22 +0100

    Remove unused search strategies.

--- src/search.hpp
+++ src/search.hpp
@@ -8,7 +8,4 @@
 #include <utility>
 #include <functional>
 
-void BFS(const Board&, int);
-void DFS(const Board&, int);
-std::pair<int, Move> negamax(const Board&, int, std::atomic<bool>&, long long);
 std::pair<int, Move> alpha_beta(Engine&, const Board&, int, int, int, std::atomic<bool>&, long long);
